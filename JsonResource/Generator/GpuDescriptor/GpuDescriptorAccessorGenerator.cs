3663812dafa9e6fa036050ec97976b1772c3ad86
tirole <60066205+tirole@users.noreply.github.com>
2020-02-03 20:22:25 +0900

    Added GpuDescriptorAccessor generator

---
+++ JsonResource/Generator/GpuDescriptor/GpuDescriptorAccessorGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonResource.Generator.GpuDescriptor
+{
+    public partial class GpuDescriptorAccessorGenerator
+    {
+        public StructGenerationInfo Info { get; }
+        public GpuDescriptorAccessorGenerator(StructGenerationInfo info)
+        {
+            this.Info = info;
+        }
+    }
+}
